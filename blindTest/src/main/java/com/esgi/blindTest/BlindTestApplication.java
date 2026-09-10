f66a2dee68d965f384271b36f22215e03f97dfc3
alexis <alexis.godard56@gmail.com>
2026-09-10 18:03:34 +0200

    Init project

---
+++ blindTest/src/main/java/com/esgi/blindTest/BlindTestApplication.java
@@ -0,0 +1,13 @@
+package com.esgi.blindTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlindTestApplication.class, args);
+	}
+
+}
---
+++ blindTest/src/test/java/com/esgi/blindTest/BlindTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgi.blindTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
